a92f4833f7fe283fb521e9625d1b81aa05f44357
mcglincy <mattmcglincy@gmail.com>
2020-11-25 22:09:47 -0600

    Add style and start tweaking sheets.

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -10,6 +10,129 @@ export class MBActorSheet extends ActorSheet {
       width: 600,
       height: 600,
       tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
+      // is dragDrop needed?
+      dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
     });
   }
+
+  /** @override */
+  getData() {
+    const data = super.getData();
+    data.dtypes = ["String", "Number", "Boolean"];
+    for (let attr of Object.values(data.data.attributes)) {
+      attr.isCheckbox = attr.dtype === "Boolean";
+    }
+
+    // Prepare items.
+    if (this.actor.data.type == 'character') {
+      this._prepareCharacterItems(data);
+    }
+
+    return data;
+  }
+
+  /**
+   * Organize and classify Items for Character sheets.
+   *
+   * @param {Object} actorData The actor to prepare.
+   *
+   * @return {undefined}
+   */
+  _prepareCharacterItems(sheetData) {
+    const actorData = sheetData.actor;
+
+    // Initialize containers.
+    var typeArrays = {
+      'armor': [],
+      'container': [],
+      'misc': [],
+      'scroll': [],
+      'weapon': [],
+    };
+
+    // Iterate through items, allocating to containers
+    for (let i of sheetData.items) {
+      let item = i.data;
+      i.img = i.img || DEFAULT_TOKEN;
+      typeArrays[i.type].push(i);
+    }
+
+    // Assign to new properties
+    actorData.armor = typeArrays['armor'];
+    actorData.containers = typeArrays['container'];
+    actorData.misc = typeArrays['misc'];
+    actorData.scrolls = typeArrays['scroll'];
+    actorData.weapons = typeArrays['weapon'];
+  }  
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Add Inventory Item
+    html.find('.item-create').click(this._onItemCreate.bind(this));
+
+    // Update Inventory Item
+    html.find('.item-edit').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      const item = this.actor.getOwnedItem(li.data("itemId"));
+      item.sheet.render(true);
+    });
+
+    // Delete Inventory Item
+    html.find('.item-delete').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      this.actor.deleteOwnedItem(li.data("itemId"));
+      li.slideUp(200, () => this.render(false));
+    });
+
+    // Handle rollable items.
+    html.find(".items .rollable").on("click", this._onItemRoll.bind(this));    
+  }
+
+  /**
+   * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  _onItemCreate(event) {
+    event.preventDefault();
+    const header = event.currentTarget;
+    // Get the type of item to create.
+    const type = header.dataset.type;
+    // Grab any data associated with this control.
+    const data = duplicate(header.dataset);
+    // Initialize a default name.
+    const name = `New ${type.capitalize()}`;
+    // Prepare the item object.
+    const itemData = {
+      name: name,
+      type: type,
+      data: data
+    };
+    // Remove the type from the dataset since it's in the itemData.type prop.
+    delete itemData.data["type"];
+
+    // Finally, create the item!
+    return this.actor.createOwnedItem(itemData);
+  }
+
+  /**
+   * Listen for roll buttons on items.
+   * @param {MouseEvent} event    The originating left click event
+   */
+  _onItemRoll(event) {
+    let button = $(event.currentTarget);
+    let r = new Roll(button.data('roll'), this.actor.getRollData());
+    const li = button.parents(".item");
+    const item = this.actor.getOwnedItem(li.data("itemId"));
+    r.roll().toMessage({
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+      flavor: `<h2>${item.name}</h2><h3>${button.text()}</h3>`
+    });
+  }   
 }
--- module/item-sheet.js
+++ module/item-sheet.js
@@ -12,5 +12,27 @@ export class MBItemSheet extends ItemSheet {
       height: 480,
       tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
     });
+  }
+
+  /** 
+   *  This is a small override to handle remembering the sheet's position.
+   *  @override 
+   */
+  setPosition(options = {}) {
+    const position = super.setPosition(options);
+    const sheetBody = this.element.find(".sheet-body");
+    const bodyHeight = position.height - 192;
+    sheetBody.css("height", bodyHeight);
+    return position;
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Roll handlers, click handlers, etc. would go here.
   }  
 }
