cfd313d76690b13ead7cbf0bd51b2c33da2c5619
mcglincy <mattmcglincy@gmail.com>
2021-01-17 21:30:06 -0600

    Implement follower sheet. Serious DRY refactoring will be needed in the future.

--- module/actor.js
+++ module/actor.js
@@ -41,4 +41,97 @@ export class MBActor extends Actor {
     const data = super.getRollData();
     return data;
   }
-}
+
+  attack(itemId) {
+    const item = this.getOwnedItem(itemId);
+    const itemRollData = item.getRollData();
+    const actorRollData = this.getRollData();
+
+    // TODO: make these multiple rolls into a single roll sheet, a la BetterRolls5e
+
+    // roll 1: attack
+    // ranged weapons use agility; melee weapons use strength
+    const isRanged = itemRollData.weaponType === 'ranged';
+    const ability = isRanged ? 'agility' : 'strength';
+    let attackRoll = new Roll(`d20+@abilities.${ability}.score`, actorRollData);
+    const weaponTypeKey = isRanged ? 'MB.Ranged' : 'MB.Melee';
+    const attackLabel = `${game.i18n.localize(weaponTypeKey)} ${game.i18n.localize('MB.Attack')}`;
+    let weaponType = 
+    attackRoll.roll().toMessage({
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker({ actor: this }),
+      flavor: `<h2>${item.name}</h2><h3>${attackLabel}</h3>`
+    });
+
+    // roll 2: damage
+    let damageRoll = new Roll("@damageDie", itemRollData);
+    let damageTitle = game.i18n.localize('MB.Damage');
+    damageTitle = damageTitle.charAt(0).toUpperCase() + damageTitle.slice(1);
+    damageRoll.roll().toMessage({
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker({ actor: this }),
+      flavor: `<h2>${item.name}</h2><h3>${damageTitle}</h2>`
+    });
+
+    // roll 3: target damage reduction
+    console.log(this.data.data.targetArmorDie);
+    if (this.data.data.targetArmorDie) {
+      let targetArmorRoll = new Roll("@targetArmorDie", actorRollData);
+      let targetArmorTitle = game.i18n.localize('MB.TargetArmor');
+      targetArmorRoll.roll().toMessage({
+        user: game.user._id,
+        speaker: ChatMessage.getSpeaker({ actor: this }),
+        flavor: `<h2>${item.name}</h2><h3><h3>${targetArmorTitle}</h3>`
+      });
+    }
+  }
+
+  defend(sheetData) {
+    let rollData = this.getRollData();
+    if (!rollData.incomingAttackDamageDie) {
+      return;
+    }
+
+    // TODO: make a fancier unified roll message w/ 3 rolls
+
+    // roll 1: defend
+    // TODO: use armor and encumberance modifiers
+    let defenseRoll = new Roll("d20+@abilities.agility.score", rollData);
+    defenseRoll.roll().toMessage({
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker({ actor: this }),
+      flavor: `<h2>${game.i18n.localize('MB.IncomingAttack')}</h2><h3>${game.i18n.localize('MB.Defend')}</h3>`
+    });
+
+    // roll 2: incoming damage
+    let damageRoll = new Roll("@incomingAttackDamageDie", rollData);
+    let damageTitle = game.i18n.localize('MB.Damage');
+    damageTitle = damageTitle.charAt(0).toUpperCase() + damageTitle.slice(1);
+    damageRoll.roll().toMessage({
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker({ actor: this }),
+      flavor: `<h2>${game.i18n.localize('MB.IncomingAttack')}</h2><h3>${damageTitle}</h3>`
+    });
+
+    // roll 3: damage reduction from equipped armor and shield
+    let damageReductionDie = "";
+    // grab equipped armor/shield, set in getData()
+    if (sheetData.data.equippedArmor) {
+      damageReductionDie = sheetData.data.equippedArmor.data.damageReductionDie;
+    }
+    if (sheetData.data.equippedShield) {
+      damageReductionDie += "+1";
+    }
+    if (damageReductionDie) {
+      let reductionRoll = new Roll("@die", {die: damageReductionDie});
+      reductionRoll.roll().toMessage({
+        user: game.user._id,
+        speaker: ChatMessage.getSpeaker({ actor: this }),
+        flavor: `<h2>${game.i18n.localize('MB.IncomingAttack')}</h2><h3>${game.i18n.localize('MB.DamageReduction')}</h3>`
+      });
+    }
+  }
+
+
+}  
+
--- module/follower-sheet.js
+++ module/follower-sheet.js
@@ -33,13 +33,58 @@
    * @return {undefined}
    */
   _prepareFollowerItems(sheetData) {
-    const actorData = sheetData.actor;
+    // TODO: refactor / DRY with character-sheet.js. Maybe move into MBActor for better reuse?
+    let equipment = [];
+    let equippedArmor = null;
+    let equippedShield = null;
+    let equippedWeapons = [];
+
     for (let i of sheetData.items) {
       let item = i.data;
       i.img = i.img || DEFAULT_TOKEN;
-      // TODO: figure out how we want to handle follower inventory
-      // do we want equipment, equippedWeapon, etc just like character?
+
+      item.equippable = (i.type === 'armor' || i.type === 'shield' || i.type === 'weapon');
+      if (item.equippable) {
+        const isEquipped = getProperty(item, "equipped");
+        item.toggleClass = isEquipped ? "equipped" : "";
+        item.toggleTitle = game.i18n.localize(isEquipped ? "MB.Equipped" : "MB.Unequipped");
+      }
+
+      if (i.type === 'armor' 
+        || i.type === 'container'
+        || i.type === 'misc'
+        || i.type === 'scroll'
+        || i.type === 'shield'
+        || i.type === 'weapon') {
+        equipment.push(i);
+      }      
+      if (i.type === 'armor') {
+        item.damageReductionDie = CONFIG.MB.armorTierDamageReductionDie[item.currentTier];
+        if (item.equipped) {
+          // only one armor may be equipped at a time
+          equippedArmor = i;
+        }
+      } else if (i.type === 'container') {
+        containers.push(i);
+      } else if (i.type === 'shield') {
+        if (item.equipped) {
+          // only one shield may be equipped at a time
+          equippedShield = i;
+        }
+      } else if (i.type === 'weapon') {
+        if (item.equipped) {
+          equippedWeapons.push(i);
+        }
+      }
     }
+    equipment.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+    equippedWeapons.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+
+    // Assign to new properties
+    sheetData.actor.data.equipment = equipment;
+    sheetData.actor.data.equippedArmor = equippedArmor;
+    sheetData.actor.data.equippedShield = equippedShield;
+    sheetData.actor.data.equippedWeapons = equippedWeapons;
   }
 
   /** @override */
@@ -54,4 +99,132 @@
     options.statusbar = false;
     super.activateEditor(name, options, initialContent);
   }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Add Inventory Item
+    html.find('.item-create').click(this._onItemCreate.bind(this));
+
+    // Update Inventory Item
+    html.find('.item-edit').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      const item = this.actor.getOwnedItem(li.data("itemId"));
+      item.sheet.render(true);
+    });
+
+    // Delete Inventory Item
+    html.find('.item-delete').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      this.actor.deleteOwnedItem(li.data("itemId"));
+      li.slideUp(200, () => this.render(false));
+    });
+
+    // Handle rollable items.
+    html.find(".attack-button").on("click", this._onAttackRoll.bind(this));
+    html.find(".defend-button").on("click", this._onDefendRoll.bind(this));
+    html.find('.item-toggle').click(this._onToggleItem.bind(this));
+    html.find('.tier-radio').click(this._onArmorTierRadio.bind(this));    
+  }
+
+  /**
+   * Handle creating a new Owned Item for the actor.
+   *
+   * @param {Event} event   The originating click event
+   * @private
+   */
+ async _onItemCreate(event) {
+    event.preventDefault();
+    const template = "systems/morkborg/templates/add-item-dialog.html";
+    let dialogData = {
+      config: CONFIG.MorkBorg
+    };
+    const html = await renderTemplate(template, dialogData);
+    return new Promise(resolve => {
+      new Dialog({
+         title: game.i18n.localize('MB.CreateNewItem'),
+         content: html,
+         buttons: {
+            create: {
+              icon: '<i class="fas fa-check"></i>',
+              label: game.i18n.localize('MB.CreateNewItem'),
+              callback: html => resolve(_createItem(this.actor, html[0].querySelector("form")))
+            },
+         },
+         default: "create",
+         close: () => resolve(null)
+        }).render(true);
+    });
+  }
+
+  /**
+   * Handle toggling the state of an Owned Item within the Actor
+   *
+   * @param {Event} event   The triggering click event
+   * @private
+   */
+  async _onToggleItem(event) {
+    console.log("******* _onToggleItem *******");
+    event.preventDefault();
+    let anchor = $(event.currentTarget);
+    const li = anchor.parents(".item");
+    const itemId = li.data("itemId");
+    const item = this.actor.getOwnedItem(itemId);
+    const attr = "data.equipped";
+    const currEquipped = getProperty(item.data, attr);
+    if (!currEquipped) {
+      // we're equipping something
+      // if this is armor or shield, unequip any other equipped armor/shield
+      if (item.type === 'armor' || item.type === 'shield') {
+        for (const otherItem of this.actor.items) {
+          if (otherItem.type === item.type && otherItem._id != item._id) {
+            const otherEquipped = getProperty(otherItem.data, attr);
+            if (otherEquipped) {
+              await otherItem.update({[attr]: false});
+            }
+          }
+        }
+      }
+    }
+    return item.update({[attr]: !getProperty(item.data, attr)});
+  }
+
+  _onAttackRoll(event) {
+    event.preventDefault();   
+    const button = $(event.currentTarget);
+    const li = button.parents(".item");
+    const itemId = li.data("itemId");
+    this.actor.attack(itemId);
+  }
+
+  _onArmorTierRadio(event) {
+    event.preventDefault();
+    let input = $(event.currentTarget);
+    let newTier = parseInt(input[0].value);
+    let li = input.parents(".item");
+    const item = this.actor.getOwnedItem(li.data("itemId"));
+    return item.update({["data.currentTier"]: newTier});
+  }
+
+  _onDefendRoll(event) {
+    event.preventDefault();  
+    let sheetData = this.getData();
+    this.actor.defend(sheetData);
+  }
 }
+
+/**
+ * Create a new Owned Item for the given actor, based on the name/type from the form.
+ */
+const _createItem = (actor, form) => {
+  const itemData = {
+    name: form.itemname.value,
+    type: form.itemtype.value,
+    data: {}
+  };
+  actor.createOwnedItem(itemData);
+};
