febdefc657acc50acd37f3f2c974e7f11d492402
mcglincy <mattmcglincy@gmail.com>
2021-01-22 20:02:01 -0600

    Add numlist as editor option.

--- module/editor.js
+++ module/editor.js
@@ -1,13 +1,12 @@
 
-
 export const setCustomEditorOptions = (options) => {
     options.relative_urls = true;
     options.skin_url = "/systems/morkborg/styles/skins/mb";
     options.skin = "morkborg";
-    options.toolbar_location = 'bottom';
-    options.plugins = 'lists table link image save';
-    options.toolbar = 'formatselect | bold italic underline strikethrough bullist image link save';
+    options.toolbar_location = "bottom";
+    options.plugins = "lists table link image save";
+    options.toolbar = "formatselect | bold italic underline strikethrough numlist bullist image link save";
     options.menubar = false;
     options.statusbar = false;
-    options.content_style = '@import url("https://fonts.googleapis.com/css2?family=Special+Elite&display=swap");';
+    options.content_style = "@import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap');";
 };
