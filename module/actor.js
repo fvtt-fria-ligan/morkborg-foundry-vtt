73700834f86022397c30a336221658469c6aec43
mcglincy <mattmcglincy@gmail.com>
2021-02-27 13:28:15 -0600

    Default newly created characters to Link Actor Data on, and vision dim 30 feet.

--- module/actor.js
+++ module/actor.js
@@ -9,6 +9,21 @@ const TEST_ABILITY_ROLL_CARD_TEMPLATE = "systems/morkborg/templates/test-ability
  */
 export class MBActor extends Actor {
   /** @override */
+  static async create(data, options={}) {
+    data.token = data.token || {};
+    if (data.type === "character") {
+      mergeObject(data.token, {
+        vision: true,
+        dimSight: 30,
+        brightSight: 0,
+        actorLink: true,
+        disposition: 1
+      }, {overwrite: false});
+    }
+    return super.create(data, options);
+  }
+
+  /** @override */
   prepareData() {
     super.prepareData();
   }
