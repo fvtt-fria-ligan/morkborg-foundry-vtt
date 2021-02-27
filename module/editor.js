998957c2f468c2f59664d1481b575e83daf90995
mcglincy <mattmcglincy@gmail.com>
2021-02-27 07:46:59 -0600

    Switch to yarn, gulp, sass.

--- module/editor.js
+++ module/editor.js
@@ -1,7 +1,7 @@
 
 export const setCustomEditorOptions = (options) => {
     options.relative_urls = true;
-    options.skin_url = "/systems/morkborg/styles/skins/mb";
+    options.skin_url = "/systems/morkborg/css/skins/mb";
     options.skin = "morkborg";
     options.toolbar_location = "bottom";
     options.plugins = "lists table link image save";
