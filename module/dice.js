86e8888ace48178eb84542429031ab44c10087f0
mcglincy <mattmcglincy@gmail.com>
2021-03-14 16:50:51 -0500

    Refactor DiceSoNice code into dice.js, and show dsn rolls for all players (via synchronize).

---
+++ module/dice.js
@@ -0,0 +1,32 @@
+/**
+ * Add a show-dice promise to the given array if Dice So Nice is available.
+ */
+ export const addShowDicePromise = (promises, roll) => {
+    if (game.dice3d) {
+      // we pass synchronize=true so DSN dice appear on all players' screens
+      promises.push(game.dice3d.showForRoll(roll, game.user, true, null, false));
+    }
+  };
+  
+/**
+ * Show roll in Dice So Nice if it's available.
+ */
+export const showDice = async (roll) => {
+  if (game.dice3d) {
+    // we pass synchronize=true so DSN dice appear on all players' screens
+    await game.dice3d.showForRoll(roll, game.user, true, null, false);
+  }
+};
+
+/**
+ * Dice sound to use for ChatMessage.
+ * False if Dice So Nice is available.
+ */
+export const diceSound = () => {
+  if (game.dice3d) {
+    // let Dice So Nice do it
+    return false;
+  } else {
+    return CONFIG.sounds.dice;
+  }
+}
