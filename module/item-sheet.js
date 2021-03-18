2baa66cea3c67143d386b06f41c1ed2b9ea9c017
mcglincy <mattmcglincy@gmail.com>
2021-03-17 22:10:56 -0500

    Make item profile img 150x150. Show item type on all item sheets.

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -10,8 +10,8 @@ export class MBItemSheet extends ItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       classes: ["morkborg", "sheet", "item"],
-      width: 520,
-      height: 480,
+      width: 600,
+      height: 500,
       tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
     });
   }
