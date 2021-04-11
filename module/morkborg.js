311047639675676cc7872512e3a537efc5c06452
mcglincy <mattmcglincy@gmail.com>
2021-04-11 16:26:17 -0500

    Position part initiative button to far left.

--- module/morkborg.js
+++ module/morkborg.js
@@ -153,7 +153,7 @@ const rollPartyInitiative = () => {
 };
 Hooks.on('renderCombatTracker', (tracker, html) => {
   const partyInitiativeButton = `<a class="combat-control" title="${game.i18n.localize('MB.RollPartyInitiative')}" dataControl="rollParty"><i class="fas fa-dice-six"></i></a>`;
-  html.find("header").find("nav").find("h3").before(partyInitiativeButton);
+  html.find("header").find("nav").last().prepend(partyInitiativeButton);
   html.find("a[dataControl=rollParty]").click(ev => { rollPartyInitiative() });
 });
 
