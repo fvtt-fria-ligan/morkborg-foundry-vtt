93a832254d9ae7056e6a5b3b6bcfc19c1ce88530
mcglincy <mattmcglincy@gmail.com>
2021-04-20 19:13:04 -0500

    Pick a random class from compendiums.

--- module/scvmfactory.js
+++ module/scvmfactory.js
@@ -1,3 +1,35 @@
 export const createScvm = async () => {
-    console.log("***** SCVM ****")
+    console.log("***** SCVM FACTORY *****")
+    const clazz = await pickRandomClass();
+    const scvm = createActorWithClass(clazz);
+};
+
+const pickRandomClass = async () => {
+    const classPacks = findClassPacks();
+    if (classPacks.length === 0) {
+        // TODO: error on 0-length classPaths
+        return;
+    }
+    const packName = classPacks[Math.floor(Math.random() * classPacks.length)];
+    const pack = game.packs.get(packName);
+    let content = await pack.getContent();
+    return content.find(i => i.data.type === "class");
+};
+
+const findClassPacks = () => {
+    const classPacks = [];
+    const packKeys = game.packs.keys();
+    for (const packKey of packKeys) {
+        // moduleOrSystemName.packName
+        const packName = packKey.split(".")[1];
+        if (packName.startsWith("class-")) {
+            // class pack
+            classPacks.push(packKey);
+        }
+    }
+    return classPacks;
+};
+
+const createActorWithClass = async (clazz) => {
+    console.log(`Creating new ${clazz.data.name}`);
 };
