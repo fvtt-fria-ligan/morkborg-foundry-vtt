193b1e07b46d8d742b69e35ca6519c172cb04cc1
mcglincy <mattmcglincy@gmail.com>
2021-12-19 22:16:13 -0600

    Localize broken dialog.

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -1153,48 +1153,35 @@ export class MBActor extends Actor {
     let additionalRolls = [];
     if (brokenRoll.total === 1) {
       const unconsciousRoll = new Roll("1d4").evaluate({ async: false });
-      const s = unconsciousRoll.total > 1 ? "s" : "";
+      const roundsWord = game.i18n.localize(unconsciousRoll.total > 1 ? "MB.Rounds": "MB.Round");
       const hpRoll = new Roll("1d4").evaluate({ async: false });
       outcomeLines = [
-        `Fall unconscious`,
-        `for ${unconsciousRoll.total} round${s},`,
-        `awaken with ${hpRoll.total} HP.`,
+        game.i18n.format("MB.BrokenFallUnconscious", {rounds: unconsciousRoll.total, roundsWord, hp: hpRoll.total})
       ];
       additionalRolls = [unconsciousRoll, hpRoll];
     } else if (brokenRoll.total === 2) {
       const limbRoll = new Roll("1d6").evaluate({ async: false });
       const actRoll = new Roll("1d4").evaluate({ async: false });
       const hpRoll = new Roll("1d4").evaluate({ async: false });
-      const s = actRoll.total > 1 ? "s" : "";
+      const roundsWord = game.i18n.localize(actRoll.total > 1 ? "MB.Rounds" : "MB.Round");
       if (limbRoll.total <= 5) {
         outcomeLines = [
-          `Broken or severed limb.`,
-          `Can't act for ${actRoll.total} round${s} then become active`,
-          `with ${hpRoll.total} HP.`,
+          game.i18n.format("MB.BrokenSeveredLimb", {rounds: actRoll.total, roundsWord, hp: hpRoll.total})
         ];
       } else {
         outcomeLines = [
-          `Lost eye.`,
-          `Can't act for ${actRoll.total} round${s} then become active with ${hpRoll.total} HP.`,
+          game.i18n.format("MB.BrokenLostEye", {rounds: actRoll.total, roundsWord, hp: hpRoll.total})
         ];
       }
       additionalRolls = [limbRoll, actRoll, hpRoll];
     } else if (brokenRoll.total === 3) {
       const hemorrhageRoll = new Roll("1d2").evaluate({ async: false });
-      const s = hemorrhageRoll.total > 1 ? "s" : "";
-      outcomeLines = [
-        `Hemorrhage:`,
-        `dead in ${hemorrhageRoll.total} hour${s}`,
-        `unless treated.`,
-        `All tests are DR16`,
-        `the first hour.`,
-      ];
-      if (hemorrhageRoll.total == 2) {
-        outcomeLines.push(`DR18 the last hour.`);
-      }
+      const hoursWord = game.i18n.localize(hemorrhageRoll.total > 1 ? "MB.Hours" : "MB.Hour");
+      const lastHour = hemorrhageRoll.total == 2 ? game.i18n.localize("MB.BrokenHemorrhageLastHour") : "";
+      outcomeLines = [game.i18n.format("MB.BrokenHemorrhage", {hours: hemorrhageRoll.total, hoursWord, lastHour})];
       additionalRolls = [hemorrhageRoll];
     } else {
-      outcomeLines = [`You are dead.`];
+      outcomeLines = [game.i18n.localize("MB.BrokenYouAreDead")];
     }
 
     const data = {
