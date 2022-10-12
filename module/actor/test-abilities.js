e7ba5bc30d3c18b863c222607b47d8264d236948
mcglincy <mattmcglincy@gmail.com>
2022-10-12 13:39:45 -0500

    Refactor ability test functions into a separate file.

---
+++ module/actor/test-abilities.js
@@ -0,0 +1,98 @@
+import { diceSound, showDice } from "../dice.js";
+
+const testAbility = async (
+  actor,
+  ability,
+  abilityKey,
+  abilityAbbrevKey,
+  drModifiers
+) => {
+  const abilityRoll = new Roll(
+    `1d20+@abilities.${ability}.value`,
+    actor.getRollData()
+  );
+  abilityRoll.evaluate({ async: false });
+  await showDice(abilityRoll);
+  const rollResult = {
+    abilityKey,
+    abilityRoll,
+    displayFormula: `1d20 + ${game.i18n.localize(abilityAbbrevKey)}`,
+    drModifiers,
+  };
+  const html = await renderTemplate(
+    "systems/morkborg/templates/chat/test-ability-roll-card.hbs",
+    rollResult
+  );
+  ChatMessage.create({
+    content: html,
+    sound: diceSound(),
+    speaker: ChatMessage.getSpeaker({ actor }),
+  });
+};
+
+export const testStrength = async (actor) => {
+  const drModifiers = [];
+  if (this.isEncumbered()) {
+    drModifiers.push(
+      `${game.i18n.localize("MB.Encumbered")}: ${game.i18n.localize(
+        "MB.DR"
+      )} +2`
+    );
+  }
+  await testAbility(
+    actor,
+    "strength",
+    "MB.AbilityStrength",
+    "MB.AbilityStrengthAbbrev",
+    drModifiers
+  );
+};
+
+export const testAgility = async (actor) => {
+  const drModifiers = [];
+  const armor = actor.equippedArmor();
+  if (armor) {
+    const armorTier = CONFIG.MB.armorTiers[armor.system.tier.max];
+    if (armorTier.agilityModifier) {
+      drModifiers.push(
+        `${armor.name}: ${game.i18n.localize("MB.DR")} +${
+          armorTier.agilityModifier
+        }`
+      );
+    }
+  }
+  if (actor.isEncumbered()) {
+    drModifiers.push(
+      `${game.i18n.localize("MB.Encumbered")}: ${game.i18n.localize(
+        "MB.DR"
+      )} +2`
+    );
+  }
+  await testAbility(
+    actor,
+    "agility",
+    "MB.AbilityAgility",
+    "MB.AbilityAgilityAbbrev",
+    drModifiers
+  );
+};
+
+export const testPresence = async (actor) => {
+  await testAbility(
+    actor,
+    "presence",
+    "MB.AbilityPresence",
+    "MB.AbilityPresenceAbbrev",
+    null
+  );
+};
+
+export const testToughness = async (actor) => {
+  await testAbility(
+    actor,
+    "toughness",
+    "MB.AbilityToughness",
+    "MB.AbilityToughnessAbbrev",
+    null
+  );
+};
