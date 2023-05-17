bea48c47c8a7922ccab9bd334b5fe20e20ee31f3
Matthew McGlincy <mattmcglincy@gmail.com>
2023-05-17 12:22:46 -0500

    Remove commented-out code.

--- module/config.js
+++ module/config.js
@@ -237,19 +237,19 @@ MB.scvmFactory = {
 
   // modules wanting to add more character classes to the generator should append uuids to this list
   classUuids: [
-    // { pack: "morkborg.class-classless-adventurer", name: "Adventurer", uuid: "Compendium.morkborg.class-classless-adventurer.p693pMIVYXMSRl8S" },
-    // { pack: "morkborg.class-esoteric-hermit", name: "Esoteric Hermit", uuid: "Compendium.morkborg.class-esoteric-hermit.ifSBk6ORiHgq3Xhr" },
-    // { pack: "morkborg.class-fanged-deserter", name: "Fanged Deserter", uuid: "Compendium.morkborg.class-fanged-deserter.2hjl45o4vXOgRgfq" },
-    // { pack: "morkborg.class-gutterborn-scum", name: "Gutterborn Scum", uuid: "Compendium.morkborg.class-gutterborn-scum.gAx8MWLiZcjiWQvc" },
-    // { pack: "morkborg.class-heretical-priest", name: "Heretical Priest", uuid: "Compendium.morkborg.class-heretical-priest.8CTmhMvQ5BJlGD2o" },
-    // { pack: "morkborg.class-occult-herbmaster", name: "Occult Herbmaster", uuid: "Compendium.morkborg.class-occult-herbmaster.59HKbdpBIXfiJt91" },
-    // { pack: "morkborg.class-wretched-royalty", name: "Wretched Royalty", uuid: "Compendium.morkborg.class-wretched-royalty.0h5xOAtzV4mm7X7D" },
+    // classless adventurer
     "Compendium.morkborg.class-classless-adventurer.p693pMIVYXMSRl8S",
+    // esoteric hermit
     "Compendium.morkborg.class-esoteric-hermit.ifSBk6ORiHgq3Xhr",
+    // fanged deserter
     "Compendium.morkborg.class-fanged-deserter.2hjl45o4vXOgRgfq",
+    // gutterborn scum
     "Compendium.morkborg.class-gutterborn-scum.gAx8MWLiZcjiWQvc",
+    // heretical priest
     "Compendium.morkborg.class-heretical-priest.8CTmhMvQ5BJlGD2o",
+    // occult herbmaster
     "Compendium.morkborg.class-occult-herbmaster.59HKbdpBIXfiJt91",
+    // wretched royalty
     "Compendium.morkborg.class-wretched-royalty.0h5xOAtzV4mm7X7D",
   ],
 };
