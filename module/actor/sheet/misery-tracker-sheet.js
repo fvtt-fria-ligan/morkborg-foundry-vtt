3de7130af07537d533102a4b6e0b5ca6062d7776
Matthew McGlincy <mattmcglincy@gmail.com>
2023-10-18 19:21:48 -0700

    Progress on Misery Tracker.

---
+++ module/actor/sheet/misery-tracker-sheet.js
@@ -0,0 +1,45 @@
+import { rollMisery } from "../../miseries.js";
+
+export class MBMiseryTrackerSheet extends ActorSheet {
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["morkborg", "sheet", "actor", "misery-tracker"],
+      width: 500,
+      height: 740,
+    });
+  }
+
+  /** @override */
+  get template() {
+    return "systems/morkborg/templates/actor/misery-tracker-sheet.hbs";
+  }
+
+  /** @override */
+  getData() {
+    const data = super.getData();
+    for (let i = 1; i <= 6; i++) {
+      const field = `misery${i}`;
+      const misery = data.data.system[field];
+      data.data.system[field].cssClass =
+        misery.psalm && misery.verse ? "activated" : "";
+    }
+    return data;
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // sheet header
+    html.find(".misery-button").on("click", this._rollMisery.bind(this));
+  }
+
+  _rollMisery(event) {
+    event.preventDefault();
+    rollMisery(this.actor);
+  }
+}
---
+++ module/miseries.js
@@ -0,0 +1,57 @@
+import { showDice } from "./dice.js";
+import { showRollResultCard } from "./utils.js";
+
+export const rollMisery = async (tracker) => {
+  const miseryRoll = new Roll(tracker.system.miseryDie || "1d6");
+  miseryRoll.evaluate({ async: false });
+  await showDice(miseryRoll);
+  const isMisery = miseryRoll.total === 1;
+  const outcomeKey = isMisery ? "MB.MiseryActivated" : "MB.MiseryNotActivated";
+  const outcomeText = game.i18n.localize(outcomeKey);
+  const data = {
+    cardTitle: game.i18n.localize("MB.MiseryCheck"),
+    rollResults: [
+      {
+        rollTitle: miseryRoll.formula,
+        roll: miseryRoll,
+        outcomeLines: [outcomeText],
+      },
+    ],
+  };
+  if (isMisery) {
+    const psalmRoll = new Roll("2d6");
+    psalmRoll.evaluate({ async: false });
+    await showDice(psalmRoll);
+    const psalm = psalmRoll.terms[0].results[0].result;
+    const verse = psalmRoll.terms[0].results[1].result;
+    const youRolled = `Psalm ${psalm}, Verse ${verse}`;
+    data.rollResults.push({
+      rollTitle: " d66 ",
+      roll: psalmRoll,
+      outcomeLines: [youRolled],
+    });
+
+    // find the next unfilled misery
+    let emptyMiseryField;
+    for (let i = 1; i <= 6; i++) {
+      const field = `misery${i}`;
+      const misery = tracker.system[field];
+      if (!(misery.psalm && misery.verse)) {
+        emptyMiseryField = field;
+        break;
+      }
+    }
+    if (emptyMiseryField) {
+      const misery = tracker.system[emptyMiseryField];
+      misery.psalm = psalm;
+      misery.verse = verse;
+      const updateObj = {};
+      updateObj[`data.${emptyMiseryField}`] = misery;
+      await tracker.update(updateObj);
+    } else {
+      // no miseries left... it is the end times
+    }
+  }
+
+  await showRollResultCard(null, data);
+};
